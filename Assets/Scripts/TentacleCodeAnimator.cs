b88e2d5b7d574da815549acab1b68c901e8cc6bc
Pedro Zorrilla <tmm.pedro.zorrilla@gmail.com>
2024-05-24 11:49:30 -0700

    Incorporated wiggle movement into the tentacle visual script Added distance based wiggle on tentacle retract Added debug methods to test launch/retract of tentacles in the art scene Moved the isconnected call o the visual script to avoid snapping into position

--- Assets/Scripts/TentacleCodeAnimator.cs
+++ Assets/Scripts/TentacleCodeAnimator.cs
@@ -2,16 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Sirenix.OdinInspector;
+using System;
+using Unity.VisualScripting;
 
 public class TentacleCodeAnimator : MonoBehaviour
 {
     [SerializeField] private TentacleVisual _visual;
-    [SerializeField] private float _launchAnimationDuration = 0.5f;
+    //TODO make sure this mathces tentacle gameplkay launch speed 
+    [SerializeField] private float _launchAnimationSpeed = 12f;
     [SerializeField] private float _retractAnimationDuration = 0.5f;
+
+    [SerializeField] private float _minDistanceForWiggle = 0.5f;
+    [SerializeField] private float _maxDistanceForWiggle = 12f;
+    [SerializeField] private Vector2 _wiggleFrequencyMinAndMaxBasedOnDistance = Vector2.zero;
+    [SerializeField] private Vector2 _wiggleAmplitudeMinAndMaxBasedOnDistance = Vector2.zero;
 
     private void Start()
     {
-        _visual = new TentacleVisual();
+        _visual = GetComponent<TentacleVisual>();
     }
     [Button]
     public void AnimateLaunch(Vector3 anchorWorldPosition)
@@ -23,30 +31,60 @@ public class TentacleCodeAnimator : MonoBehaviour
     {
         StartCoroutine(RetractTentacle());
     }
+    private void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("Mouse left click");
+            Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            mousePosition.z = 0;
+            AnimateLaunch(mousePosition);
+            Debug.Log("Distance of launch is : " +Vector3.Distance(transform.position, mousePosition));
+        }
+        if (Input.GetMouseButtonDown(1))
+        {
+            Debug.Log("Mouse right click");
+            AnimateRetract();
+        }
+    }
 
     private IEnumerator LaunchTentacle(Vector3 anchorWorldPosition)
     {
         float lerp = 0;
+        _visual.IsLaunching = true;
         while (lerp < 1)
         {
-            lerp += (1/_launchAnimationDuration) * Time.deltaTime;
-            Vector3 finalPosition = Vector3.Lerp(transform.position, anchorWorldPosition, lerp);
+            lerp += _launchAnimationSpeed * Time.deltaTime;
+            Vector3 finalPosition = Vector3.Slerp(Vector3.zero, anchorWorldPosition, lerp);
             _visual.SetFollowEndPosition(finalPosition);
             yield return new WaitForEndOfFrame();
         }
-        _visual.SetIsConnecteed(true);
     }
 
     private IEnumerator RetractTentacle()
     {
         _visual.SetIsConnecteed(false);
+        _visual.SetIsWiggling(true);
         float lerp = 0;
+        Vector3 startPos = _visual.FollowTransform.position;
         while (lerp < 1)
         {
-            lerp += (1 / _launchAnimationDuration) * Time.deltaTime;
-            Vector3 finalPosition = Vector3.Lerp(_visual.FollowTransform.position, transform.position, lerp);
+            lerp += (1 / _retractAnimationDuration) * Time.deltaTime;
+            Vector3 finalPosition = Vector3.Lerp(startPos, Vector3.zero, lerp);
+            SetDistanceBasedWiggle();
             _visual.SetFollowEndPosition(finalPosition);
             yield return new WaitForEndOfFrame();
         }
+        _visual.SetIsWiggling(false);
+    }
+
+    private void SetDistanceBasedWiggle()
+    {
+        float distance = Vector3.Distance(_visual.transform.position, _visual.FollowTransform.position);
+        float lerp = Mathf.InverseLerp(_maxDistanceForWiggle, _minDistanceForWiggle, distance);
+        lerp = distance > _maxDistanceForWiggle ? 1 : lerp;
+        float distanceBasedWaveMagnitude = Mathf.Lerp(_wiggleAmplitudeMinAndMaxBasedOnDistance.y, _wiggleAmplitudeMinAndMaxBasedOnDistance.x, lerp);
+        float distanceBasedWaveFrequency = Mathf.Lerp(_wiggleFrequencyMinAndMaxBasedOnDistance.y, _wiggleFrequencyMinAndMaxBasedOnDistance.x, lerp);
+        _visual.SetWiggleVariables(distanceBasedWaveFrequency, distanceBasedWaveMagnitude);
     }
 }
