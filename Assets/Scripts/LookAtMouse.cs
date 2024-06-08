eae43d7b5997ccf91469966314aa79671fb8cf11
Pedro Zorrilla <tmm.pedro.zorrilla@gmail.com>
2024-06-08 12:54:29 -0700

    Added a little animated head to octopus guy!

--- Assets/Scripts/LookAtMouse.cs
+++ Assets/Scripts/LookAtMouse.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Sirenix.OdinInspector;
 
 public class LookAtMouse : MonoBehaviour
 {
@@ -10,7 +11,12 @@ public class LookAtMouse : MonoBehaviour
 
     private Vector3 _lookDirection;
     [SerializeField] private float _crossProductSign = 1;
+    private Animator _animator;
 
+    private void Start()
+    {
+        _animator = GetComponent<Animator>();
+    }
     private void Update()
     {
         _lookDirection = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
@@ -31,10 +37,19 @@ public class LookAtMouse : MonoBehaviour
         normalizedNormalSum = normalizedNormalSum.normalized;
         Vector3 directionCrossProduct = Vector3.Cross(normalizedNormalSum, _lookDirection.normalized);
         //Debug.Log(Vector3.Cross(normalizedNormalSum, _lookDirection.normalized));
-        _crossProductSign = Mathf.Sign(directionCrossProduct.z) * -1;
-        if (_crossProductSign != transform.localScale.x)
-        {
-            transform.localScale = new Vector3(_crossProductSign, 1, 1);
-        }
+        float newCrossProduct = Mathf.Sign(directionCrossProduct.z);
+        _animator.SetInteger("Direction", (int)newCrossProduct);
+
+        //if (_crossProductSign != newCrossProduct)
+        //{
+        //    _crossProductSign = newCrossProduct;
+        //    _animator.SetInteger("Direction", (int)_crossProductSign);
+        //}
     }
+    [Button]
+    private void AnimateFlip()
+    {
+        _animator.SetInteger("Direction", (int)_crossProductSign);
+    }
+
 }
